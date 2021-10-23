df5cb051a01baa82c8b625c925f7d6cbcdb05102
ZorinIvanA <zorin.ivan@outlook.com>
2021-10-23 13:09:11 +0300

    OOP & database

---
+++ OOP/OOP/OOP/Animal/Mammal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Animal
+{
+    public class Mammal : Animal
+    {
+        public void SetVaccine() { }
+    }
+}
---
+++ OOP/OOP/OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using OOP.Animal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lesson lesson = new Lesson();
+            //lesson.Finish(DateTime.Now);
+            //lesson.Start();
+            //var tempDate = lesson.GetFinishDate();
+            //Console.WriteLine(tempDate);
+
+            //var animal = new Bird();
+            //animal.Diagnose();
+
+            var lesson = new Lesson();
+            lesson.ClassNumber = 1;
+
+            Console.ReadLine();
+        }
+    }
+}
